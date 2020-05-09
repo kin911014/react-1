f3cd6b108ac306a9cf0d19b121f1c15325a9aead
kin911014 <50545361+kin911014@users.noreply.github.com>
2020-05-09 15:24:49 +0900

    fix header css #3

--- src/header/Header.js
+++ src/header/Header.js
@@ -9,9 +9,14 @@ export default class header extends React.Component {
       <div className="header">
         <div className="header_wrapper">
           <div className="logo-header">
-            <img className="logo" src="http://localhost:8090/images/logo.png" />
-            <div className="logo-header-title">
-              <p>ShareEditor.Md</p>
+            <div>
+              <img
+                className="logo"
+                src="http://localhost:8090/images/logo.png"
+              />
+              <div className="logo-header-title">
+                <p>ShareEditor.Md</p>
+              </div>
             </div>
           </div>
           <div className="search_div">
