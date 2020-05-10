4c9f6491a4c96c7190ddcde1d03336c86707259c
kin911014 <50545361+kin911014@users.noreply.github.com>
2020-05-10 19:19:47 +0900

    fix header css #8
    fix header bug

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default class Footer extends React.Component {
   render() {
     return (
-      <div className="footer">
+      <div className="Footer">
         <p>(c)opyright 2015, 2016, 2017, 2018, 2019, 2020</p>
       </div>
     );
